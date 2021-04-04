85ee3db96a76616a987be265e62ba9452a22055d
AmoghaKS <amoghaks10@gmail.com>
2021-04-04 10:26:41 +0530

    capture payload context in frontend

--- frontend/src/Components/Component.jsx
+++ frontend/src/Components/Component.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import BotPayloadContext from '../BotPayloadContext';
 
-function Component({id, image, title, displayText, button}) {
+function Component({id, image, title, displayText, button, componentPayload}) {
+    const {value,setValue} = useContext(BotPayloadContext);
+
+
     return(
         <div id={id}>
             <div className='landing-page-image'>
@@ -9,7 +13,8 @@ function Component({id, image, title, displayText, button}) {
             <div className='landing-page-text'>
                 <h2> {title} </h2>
                 <p>{displayText}</p>
-                <button>{button}</button>
+                <button onClick={() => setValue(componentPayload)}>{button}</button>
+                {/* <h1>{value}</h1> */}
             </div>
         </div>
     )
